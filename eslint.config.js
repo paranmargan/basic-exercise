bd17a3257b3439b9e67a087768f212c84499ee80
psparan1980 <psparan1980@gmail.com>
2026-03-28 08:00:32 +0530

    update to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,22 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ]);
